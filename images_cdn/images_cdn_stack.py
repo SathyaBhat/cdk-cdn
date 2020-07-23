46e59dd786c790aa0fe54be4119f14fd6c6f8965
Sathyajith <sabhat@adobe.com>
2020-07-23 09:52:42 +0300

    add Cloudfront + ACM

--- images_cdn/images_cdn_stack.py
+++ images_cdn/images_cdn_stack.py
@@ -1,11 +1,39 @@
 from aws_cdk import core
 from aws_cdk import aws_s3 as s3
-
-
+from aws_cdk import aws_cloudfront as cf
+from aws_cdk import aws_certificatemanager as acm
 class ImagesCdnStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, bucket_name: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-        s3.Bucket(self, "images-cdn", bucket_name=bucket_name)
+        bucket = s3.Bucket(self, 
+                            "images-cdn",
+                            bucket_name=bucket_name,
+                            access_control=s3.BucketAccessControl.PUBLIC_READ,
+                            website_index_document="index.html"
+                            )
+        cert = acm.Certificate(self, 
+                        "Cert", 
+                        domain_name=bucket_name,
+                        validation=acm.CertificateValidation.from_dns()
+                        )
+        
+        cf.CloudFrontWebDistribution(self, "cf_cdn",
+                                     price_class=cf.PriceClass.PRICE_CLASS_200,
+                                     origin_configs=[
+                                         cf.SourceConfiguration(
+                                             behaviors=[
+                                                 cf.Behavior(
+                                                     is_default_behavior=True)
+                                             ],
+                                             s3_origin_source=cf.S3OriginConfig(
+                                                 s3_bucket_source=bucket
+                                             )
+                                         )
+                                     ],
+                                     alias_configuration=cf.AliasConfiguration(names=[bucket_name],
+                                                                acm_cert_ref=cert.certificate_arn
+                                         )
+                                    )
